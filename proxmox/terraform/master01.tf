9aa64facae3548bc7ff0ed9974d348b9b5b623de
brian <blobo@redhat.com>
2023-06-25 18:08:32 -0300

    Se sube ejemplo de docker non-root

--- proxmox/terraform/master01.tf
+++ proxmox/terraform/master01.tf
@@ -3,29 +3,20 @@ resource "proxmox_vm_qemu" "ubuntu-kube-m1" {
   name = "kube-m1"
   desc = "ubuntu-server for kubernetes"
   tags = "kube,master"
+  vmid = "101"
 
-  vmid        = "110"
   target_node = "server01"
   clone       = "kube"
 
-  os_type = "cloud-init"
-  agent   = 0
+  os_type      = "cloud-init"
+  agent        = 0
+  ipconfig0    = "ip=192.168.122.101/24,gw=192.168.122.1"
+  ciuser       = "kube"
+  cicipassword = "kube"
 
   startup = "1"
   cores   = 2
   sockets = 2
   memory  = 4096
-
-  network {
-    model    = "virtio"
-    bridge   = "vmbr0"
-    firewall = true
-  }
-
-  disk {
-    type    = "scsi"
-    storage = "local-lvm"
-    size    = "32G"
-  }
 
 }
